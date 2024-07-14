94247b017092e9ec7c7042983279e8a4067a094e
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-14 13:09:05 +0200

    A player has a size, code by its Transform's scale

--- src/app.rs
+++ src/app.rs
@@ -49,7 +49,7 @@ fn add_player(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                size: Vec3::new(64.0, 32.0, 1.0),
+                scale: Vec3::new(64.0, 32.0, 1.0),
                 ..default()
             },
             ..default()
